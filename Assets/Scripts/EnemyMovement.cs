a6e94b49d25e6bda552e8d837ce10b46d85a3e7a
Gillian <gillibeanbuckelew@gmail.com>
2018-12-07 16:07:43 -0600

    Vid 156 completed
    Short time to work
    Changed path and made base explode when hit

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
+
     [SerializeField] float movementperiod = .5f;
+
+    [SerializeField] ParticleSystem deathParticle;
+
+
     void Start () {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
         var path = pathfinder.GetPath();
@@ -16,5 +21,18 @@ public class EnemyMovement : MonoBehaviour {
             transform.position = waypoint.transform.position;
             yield return new WaitForSeconds(movementperiod);
         }
+        KillEnemy();
+    }
+    private void KillEnemy()
+    {
+        //play a sound when enemy dies
+        Vector3 deathPos = new Vector3(transform.position.x, transform.position.y + 20, transform.position.z);
+        var death = Instantiate(deathParticle, deathPos, Quaternion.identity);
+
+        death.Play();
+        float destroyDelay = death.main.duration;
+        Destroy(death.gameObject, destroyDelay);
+
+        Destroy(gameObject);
     }
 }
