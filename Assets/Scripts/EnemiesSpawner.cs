5646bebfc0a519e0c54093605ea283639867ebe1
Gillian <gillibeanbuckelew@gmail.com>
2018-12-10 15:10:11 -0600

    Vid 159
    -Changed Lighting & Background
    -Added UI Score & Health

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemiesSpawner : MonoBehaviour {
     [Range(0.1f,60f)] [SerializeField] float secondsBetweenSpawns= .5f;
     [SerializeField] EnemyCollision enemyPrefab;
     [SerializeField] Transform parent;
-	
-	void Start () {
+    [SerializeField] int enemiesSpawned=0;
+    [SerializeField] Text enemiesText;
+
+    void Start () {
         StartCoroutine(RepeatedSpawnEnemies());
+        enemiesText.text = enemiesSpawned.ToString();
 	}
     IEnumerator RepeatedSpawnEnemies()
     {
         while (true)//forever
         {
-            var newEnemy= Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             newEnemy.transform.parent = parent;
+            EnemySpawned();
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    private void EnemySpawned()
+    {
+        enemiesSpawned++;
+        enemiesText.text = enemiesSpawned.ToString();
     }
 }
